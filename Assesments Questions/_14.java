c00e626afa16b39f13531e103cba60c7ca868261
karan singh D <ceitkaransingh2026@gmail.com>
2025-02-28 22:51:23 +0530

    Question no 28

--- Assesments Questions/_14.java
+++ Assesments Questions/_14.java
@@ -1,6 +1,41 @@
 // 14.Multidimensional array [Matrix] backtracking problem
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+
 public class _14 {
   public static void main(String[] args) {
 
+    // try {
+    //   BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+
+    //   int num=Integer.parseInt(reader.readLine());
+  
+    //   System.out.println(num+"jndje");
+    // } catch (Exception e) {
+  
+    //   System.out.println(e);
+    // }
+
+    try {
+      BufferedReader sb=new BufferedReader(new InputStreamReader(System.in));
+      String name=sb.readLine();
+
+      System.out.println(name);
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+ 
+        // try {
+        //     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        //     System.out.print("Enter your name: ");
+        //     String name = reader.readLine();
+        //     System.out.println("Hello, " + name);
+        // } catch (IOException e) {
+        //     System.out.println("Error reading input: " + e.getMessage());
+        // }
   }
 }
--- Assesments Questions/_28.java
+++ Assesments Questions/_28.java
@@ -6,6 +6,48 @@
 // arr = [38, 19, 77, 56, 45, 92, 100] 
 // Output : 
 // 2
+import java.util.HashMap;
+import java.util.Map;
+
 public class _28 {
-  
+   
+    private static int getSingleDigit(int num) {
+        while (num >= 10) {
+            int sum = 0;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+
+    private static int findMostFrequentDigit(int[] numbers) {
+        Map<Integer, Integer> digitFrequency = new HashMap<>();
+        
+        for (int num : numbers) {
+            int singleDigit = getSingleDigit(num);
+            digitFrequency.put(singleDigit, digitFrequency.getOrDefault(singleDigit, 0) + 1);
+        }
+        
+        int maxCount = 0;
+        int result = 9; 
+        
+        for (Map.Entry<Integer, Integer> entry : digitFrequency.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if (value > maxCount || (value == maxCount && key < result)) {
+                maxCount = value;
+                result = key;
+            }
+        }
+        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {38, 19, 77, 56, 45, 92, 100};
+        System.out.println(findMostFrequentDigit(numbers));
+    }
 }
